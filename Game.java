8924adb01011f3c4bbe2d2c3d99de425a94e43e0
Triplett2008 <WILLIAM.TRIPLETT@jpschools.org>
2026-03-06 11:11:43 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,31 @@
 public class Game {
+    private String name;
+    private int happinessIncr;
+    private int weightDecr;
     
+  public Game (String name, int happinessIncr, int weightDecr) {
+      this.name = name;
+      this.happinessIncr = happinessIncr;
+      this.weightDecr = weightDecr;
+  }
+  
+  public String getName () {
+      return name;
+  }
+  
+  public int getHappinessIncr () {
+      return happinessIncr;
+  }
+  
+  public int getWeightDecr () {
+      return weightDecr;
+  }
+  
+  public boolean isWinner () {
+      if (Math.random()>0.5) {
+          return true;
+      } else {
+          return false;
+      }
+  }
 }
